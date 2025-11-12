36a027126845497db728d4a352f9e28bb1edbe81
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-11-12 11:21:47 +0545

    task1 completed

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace task1
+﻿using System.Text.Json.Serialization.Metadata;
+
+namespace task1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, from task 1");
+
+            var ops = new Operators();
+
+            int a = 26;
+            int b = 4;
+            int number = 15;
+
+            Console.WriteLine($"Add: {ops.Add(a, b)}");
+            Console.WriteLine($"Subtract: {ops.Subtract(a, b)}");
+            Console.WriteLine($"Multiply: {ops.Multiply(a, b)}");
+            Console.WriteLine($"Divide: {ops.Divide(a, b)}");
+
+            ops.OddEvenFinder(number);
         }
     }
 }
