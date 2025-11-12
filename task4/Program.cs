c342963ce68eb5f9cd08a535503c169c4f547e41
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-11-12 11:47:03 +0545

    task4 completed

---
+++ task4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from task4");
+
+            Console.WriteLine("Enter day as number (1-7): ");
+
+            int day = Convert.ToInt32(Console.ReadLine());
+            string dayName = day switch
+            {
+                1 => "Sunday",
+                2 => "Monday",
+                3 => "Tuesday",
+                4 => "Wednesday",
+                5 => "Thursday",
+                6 => "Friday",
+                7 => "Saturday",
+                _ => "Invalid day number"
+            };
+
+            Console.WriteLine($"Day is: {dayName}");
+
+        }
+    }
+}
