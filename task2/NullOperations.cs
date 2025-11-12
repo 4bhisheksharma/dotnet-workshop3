1f45b672a6c1679434ee01e72f20d723d8ee3dc2
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-11-12 11:33:57 +0545

    task2 completed

---
+++ task2/NullOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class NullOperations
+    {
+
+        public void PerformNullChecks()
+        {
+            string? username = null;
+
+            string checkNull = (username == null) ? "Username is not available" : username;
+            Console.WriteLine("Ternary Operator: " + checkNull);
+
+            string checkNullByNonCoalesc = username ?? "Username is not available";
+            Console.WriteLine("Null Coalescing Operator: " + checkNullByNonCoalesc);
+
+            username ??= "Rambahadur";
+            Console.WriteLine("Null Coalescing Assignment: " + username);
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from task2");
+
+            var nullOps = new NullOperations();
+
+            nullOps.PerformNullChecks();
+        }
+    }
+}
